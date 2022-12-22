cbe39d3e1cfdc5c14d1aae4fb175cb4ef7ce0a3b
eduado <eduardo.hor@outlook.com>
2022-12-22 18:24:18 -0300

    :heavy_plus_sign: feat: Adding login method rules

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,12 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Termwind\Components\Raw;
 
 class AuthController extends Controller
 {
-    public function login(){
-        return 'login';
+    public function register(RegisterRequest $request){
+        return $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+    }
+
+    public function login(LoginRequest $request){
+
+        $credentials = $request->all();
+        if(!$token = auth('api')->attempt($credentials)){
+            return response()->json(['error' => 'Usuário ou senha inválido!'], 403);
+        };
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer'
+        ]);
     }
 
     public function logout(){
