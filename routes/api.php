2d3d569e28146c672197ffa6b57a688af323c600
eduado <eduardo.hor@outlook.com>
2022-12-22 23:52:18 -0300

    :heavy_plus_sign: feat: adding me and refresh methods

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,20 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
+Route::prefix('v1')->group(function(){
 
-Route::middleware('jwt.auth')->prefix('v1')->group(function(){
-  Route::post('logout' , [AuthController::class, 'logout']);
-  Route::post('refresh', [AuthController::class, 'refresh']);
-  Route::post('me', [AuthController::class, 'me']);
-
+  Route::post('register', [AuthController::class, 'register']);
+  Route::post('login', [AuthController::class, 'login']);
+  
+  Route::middleware('jwt.auth')->group(function(){
+    Route::post('logout' , [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+  
+  });
 });
+
 
 
