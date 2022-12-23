8ca03cd8f43705b95dcde9be108fbd19091e0a44
eduado <eduardo.hor@outlook.com>
2022-12-22 23:22:54 -0300

    :heavy_plus_sign: feat: Adding prefix and route protection

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,9 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Termwind\Components\Raw;
 
 class AuthController extends Controller
 {
--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
-Route::post('logout' , [AuthController::class, 'logout']);
-Route::post('refresh', [AuthController::class, 'refresh']);
-Route::post('me', [AuthController::class, 'me']);
+
+Route::middleware('jwt.auth')->prefix('v1')->group(function(){
+  Route::post('logout' , [AuthController::class, 'logout']);
+  Route::post('refresh', [AuthController::class, 'refresh']);
+  Route::post('me', [AuthController::class, 'me']);
+
+});
+
 
